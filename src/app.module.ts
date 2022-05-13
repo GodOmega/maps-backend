6d97e0236a86a5387123dd63540aa640c0d5c0de
= <=>
2022-05-13 18:25:20 -0400

    maps backend

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { WorkersGateway } from './workers/workers.gateway';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, WorkersGateway],
+})
+export class AppModule {}
---
+++ src/workers/workers.gateway.ts
@@ -0,0 +1,37 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+
+@WebSocketGateway(81, {
+  cors: { origin: '*' },
+})
+export class WorkersGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+
+  afterInit(server: any) {
+    console.log('workers socket on')
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log('alguien se conecto')
+  }
+
+  handleDisconnect(client: any) {
+    console.log('alguien se desconecto')
+  }
+
+  @SubscribeMessage('join_work')
+  handleMessage(client: any, payload: any): void {
+    console.log('joined')
+  }
+
+  @SubscribeMessage('disconnect_work')
+  handleDisconnectWork(client: any, payload: any): void {
+    console.log(payload)
+  }
+}
