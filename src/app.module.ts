6bc5a32a0fd994012e9ab0b932f7f68936d4fea8
= <=>
2022-05-13 18:59:29 -0400

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WorkersGateway } from './workers/workers.gateway';
+// import { WorkersGateway } from './workers/workers.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, WorkersGateway],
+  providers: [AppService],
 })
 export class AppModule {}
