e1a513232bb6bf9140ed98322e036bb46f73e877
= <=>
2022-05-23 20:43:45 -0400

    create users

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { UsersService } from '../services/users.service';
+import { CreateUserDto } from '../dtos/users.dto';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +10,10 @@ export class UsersController {
   @Get()
   findAll() {
     return this.userService.findAll();
+  }
+
+  @Post()
+  create(@Body() payload: CreateUserDto) {
+    return this.userService.create(payload)
   }
 }
